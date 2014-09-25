dff7fbf53149bbf8babac7ba87c9e675b690faa7
Abin <abin.abraham4@gmail.com>
2014-09-25 17:48:40 -0400

    Create index feature for restaurants

--- spec/requests/restaurant_spec.rb
+++ spec/requests/restaurant_spec.rb
@@ -1,19 +1,8 @@
 require 'spec_helper'
 
-describe RestaurantsController do
-  context "GET /restaurants/:id" do
-    before do
-      @restaurant = FactoryGirl.create :restaurant
-    end
-
-    context "resource exists" do
-      subject {get "restaurants/#{@restaurant.id}"}
-      it { expect(subject).to render_template(:show)}
-    end
-
-    context "resource doesn't exist" do
-      subject {get "restaurants/#{@restaurant.id + 1}"}
-      it { expect(subject).to redirect_to(:root) }
-    end
+context "GET /" do
+  subject { get "/" }
+  it "renders index" do
+    expect(subject).to render_template(:index)
   end
 end
